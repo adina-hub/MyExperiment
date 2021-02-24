f77c40b4e71a4463e80cf8254b69f8cd4e76e24e
andreimediatack <andrei.manea@mediatack.de>
2021-02-24 10:21:21 +0200

    Rearranged global styles.

--- src/globalStyles.js
+++ src/globalStyles.js
@@ -1,10 +1,14 @@
-export const buttonColor = '#4d194d';
-export const fontSizeH2 = '23px';
-export const fontSizeH3 = '16px';
-export const fontSizeP = '14px';
+// COLORS
+export const purple = '#4d194d';
+export const green = '#8BD8BD';
+export const turquoise = '#51A3B9';
+// BACKGROUNDS
 export const lightBg = '#31304c';
 export const darkBg = '#272640';
-export const turqBg = '#51A3B9';
-export const purple = '#4d194d';
-export const turquoise = '#8BD8BD';
-export const buttonText = '13px';
+// FONTS
+export const fontH2 = '23px';
+export const fontH3 = '16px';
+export const fontP = '14px';
+// CONTAINERS
+
+

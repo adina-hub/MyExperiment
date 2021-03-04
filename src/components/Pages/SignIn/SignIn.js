a4a88b82a5a52d411273a9f1473ce4a448f77b31
andreimediatack <andrei.manea@mediatack.de>
2021-03-04 14:17:32 +0200

    Defined components for auth pages and modified Login accordingly.

--- src/components/Pages/SignIn/SignIn.js
+++ src/components/Pages/SignIn/SignIn.js
@@ -1,257 +1,48 @@
 import React from 'react';
 import logo from '../../../images/logo.svg';
-import styled from 'styled-components';
-import {
-	fontH3,
-	lightBg,
-	fontH2,
-	fontH5,
-	fontP,
-	turquoise
-} from '../../../globalStyles';
-import { Form, Field, Formik } from 'formik';
-import { BsPerson, BsLock } from 'react-icons/bs';
-import { Link as LinkR } from 'react-router-dom';
-
+import { Formik } from 'formik';
+import { Link } from 'react-router-dom';
+import { AuthBtn, AuthContainer, AuthForm, AuthFormContainer, AuthHelper, AuthInputContainer, AuthInputField, AuthLabel, AuthLink, AuthLogo, AuthTitle, MailIcon, PassIcon } from '../../../styles/auth';
 export default function SignIn() {
 	return (
-		<>
-			<PageContainer>
-				<img src={logo} alt="" />
-				<FormContainer>
-					<h3>Login</h3>
-					<Formik
-						initialValues={{ email: '', password: '' }}
-						onSubmit={async (values) => {
-							await new Promise((resolve) => setTimeout(resolve, 500));
-							alert(JSON.stringify(values, null, 2));
-						}}
-					>
-						<SignInForm>
-							<label id="email">Email</label>
-							<InputField>
-								<UserIcon></UserIcon>
-								<Input
-									name="email"
-									type="email"
-									placeholder="Type your email"
-									required
-								/>
-							</InputField>
-
-							<label id="password">Password</label>
-							<InputField>
-								<PassIcon></PassIcon>
-								<Input
-									name="password"
-									type="password"
-									placeholder="Type your password"
-									required
-								/>
-							</InputField>
-
-							<a href="/">Forgot password?</a>
-							<button type="submit">LOGIN</button>
-							<Text>
-								Need an account? <Link to="/signUp">Register</Link>
-							</Text>
-						</SignInForm>
-					</Formik>
-				</FormContainer>
-			</PageContainer>
-		</>
+		<AuthContainer>
+			<AuthLogo src={logo} alt="" />
+			<AuthFormContainer dark="true">
+				<AuthTitle>Login</AuthTitle>
+				<Formik
+					initialValues={{ email: '', password: '' }}
+					onSubmit={async (values) => {
+						await new Promise((resolve) => setTimeout(resolve, 500));
+						alert(JSON.stringify(values, null, 2));
+					}}
+				>
+					<AuthForm>
+						<AuthLabel>Email</AuthLabel>
+						<AuthInputContainer>
+							<MailIcon />
+							<AuthInputField
+								name="email"
+								type="email"
+								placeholder="Type your email"
+								required />
+						</AuthInputContainer>
+						<AuthLabel>Password</AuthLabel>
+						<AuthInputContainer>
+							<PassIcon />
+							<AuthInputField
+								name="password"
+								type="password"
+								placeholder="Type your password"
+								required />
+						</AuthInputContainer>
+						<AuthLink>Forgot password?</AuthLink>
+						<AuthBtn type="submit">LOGIN</AuthBtn>
+					</AuthForm>
+				</Formik>
+				<AuthHelper>Need an account?{' '}<Link>Register now</Link></AuthHelper>
+			</AuthFormContainer>
+		</AuthContainer>
 	);
 }
 
-const PageContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	height: 100vh;
-	background-color: ${lightBg};
 
-	> img {
-		width: 110px;
-		height: 110px;
-		margin-left: 8px;
-
-		@media screen and (min-width: 768px) {
-			width: 120px;
-			height: 120px;
-			margin-left: 17px;
-		}
-
-		@media screen and (min-width: 1024px) {
-			width: 140px;
-			height: 140px;
-			margin-left: 30px;
-		}
-	}
-`;
-
-const FormContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	margin: 70px auto;
-	height: 53vh;
-	width: 85vw;
-	border-radius: 8px;
-	padding: 22px;
-	background: ${turquoise};
-
-	@media screen and (min-width: 375px) {
-		width: 75vw;
-	}
-
-	@media screen and (min-width: 768px) {
-		width: 45vw;
-		height: 58vh;
-		margin: 50px auto;
-	}
-
-	@media screen and (min-width: 1024px) {
-		width: 35vw;
-		height: 60vh;
-		margin: 25px auto;
-	}
-
-	@media screen and (min-width: 1200px) {
-		width: 30vw;
-		height: 67vh;
-		margin: 20px auto;
-		padding: 25px;
-	}
-
-	> h3 {
-		font-size: ${fontH3};
-		color: ${lightBg};
-		font-weight: 700;
-		text-align: center;
-
-		@media screen and (min-width: 1200px) {
-			font-size: ${fontH2};
-		}
-	}
-`;
-
-const SignInForm = styled(Form)`
-	display: flex;
-	flex-direction: column;
-
-	> label {
-		margin-top: 10px;
-		font-size: ${fontH5};
-		font-weight: 400;
-		padding: 10px 5px;
-		color: #333333;
-
-		@media screen and (min-width: 768px) {
-			font-size: ${fontH3};
-		}
-
-		@media screen and (min-width: 1200px) {
-			font-size: ${fontH3};
-		}
-	}
-
-	> a {
-		margin-top: 10px;
-		margin-bottom: 25px;
-		opacity: 0.6;
-		font-size: ${fontH5};
-		color: white;
-		text-align: right;
-		cursor: pointer;
-		&:hover {
-			color: blue;
-			transition: 0.15s ease-in-out;
-		}
-
-		@media screen and (min-width: 768px) {
-			margin-top: 13px;
-			font-size: ${fontH3};
-		}
-
-		@media screen and (min-width: 1024px) {
-			margin-top: 17px;
-		}
-	}
-
-	> button {
-		padding: 10px;
-		border: none;
-		border-radius: 12px;
-		color: #fff;
-		background: ${lightBg};
-		font-size: ${fontP};
-		cursor: pointer;
-		text-decoration: none;
-		text-align: center;
-	}
-`;
-
-const InputField = styled.div`
-	display: flex;
-	border: none;
-	border-bottom: 1px solid #d9d9d9;
-	padding: 10px 5px;
-`;
-
-const UserIcon = styled(BsPerson)`
-	color: white;
-	margin-right: 10px;
-`;
-
-const Input = styled(Field)`
-	border: none;
-	background: none;
-	width: 100%;
-	cursor: pointer;
-	::placeholder {
-		color: white;
-		opacity: 0.6;
-	}
-
-	&:focus {
-		outline: none;
-		::placeholder {
-			color: ${turquoise};
-		}
-	}
-
-	@media screen and (min-width: 768px) {
-		font-size: ${fontP};
-	}
-
-	@media screen and (min-width: 1200px) {
-		font-size: ${fontH3};
-	}
-`;
-
-const PassIcon = styled(BsLock)`
-	color: white;
-	margin-right: 10px;
-`;
-
-const Text = styled.div`
-	display: flex;
-	font-size: ${fontH5};
-	margin-top: 12px;
-	justify-content: center;
-
-	@media screen and (min-width: 768px) {
-		margin-top: 30px;
-		font-size: ${fontH3};
-	}
-`;
-
-const Link = styled(LinkR)`
-	margin-left: 7px;
-	color: ${lightBg};
-	cursor: pointer;
-	font-weight: bold;
-	text-decoration: none;
-	&:hover {
-		color: blue;
-		transition: 0.15s ease-in-out;
-	}
-`;
---
+++ src/styles/auth.js
@@ -0,0 +1,143 @@
+import styled from 'styled-components';
+import { Field, Form } from 'formik';
+import { darkBg, fontH1, fontH3, fontP, green, lightBg } from './general';
+import { Link } from 'react-router-dom';
+import MailOutlineOutlinedIcon from '@material-ui/icons/MailOutlineOutlined';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
+//Containers 
+export const AuthContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	background: ${darkBg};
+	height: 100vh;
+`
+
+export const AuthFormContainer = styled.div`
+	background: ${lightBg};
+	width: 85%;
+	padding: 30px 20px;
+	margin: 30px auto;
+	border-radius: 15px;
+`;
+
+export const AuthInputContainer = styled.div`
+	display: flex;
+	border: none;
+	border-bottom: 1px solid #d9d9d9;
+	padding: 10px 5px;
+`
+
+//Form 
+export const AuthForm = styled(Form)`
+	display: flex;
+	flex-direction: column;
+`
+
+export const AuthLabel = styled.label`
+		margin-top: 10px;
+		font-size: 12px;
+		font-weight: 400;
+		padding: 5px;
+		color: ${green};
+		font-size: ${fontH3};
+`
+
+export const AuthInputField = styled(Field)`
+	border: none;
+	background: none;
+	width: 100%;
+	cursor: pointer;
+	caret-color: ${green};
+	color: white;
+	::placeholder {
+		color: white;
+		opacity: 0.6;
+	}
+
+	&:focus {
+		outline: none;
+		::placeholder {
+			color: gray;
+		}
+	}
+`
+
+//Logo
+
+export const AuthLogo = styled.img`
+		width: 130px;
+		height: 130px;
+		margin-left: 8px;
+
+		@media screen and (min-width: 768px) {
+			width: 150px;
+			height: 150px;
+			margin-left: 17px;
+		}
+
+		@media screen and (min-width: 1024px) {
+			width: 180px;
+			height: 180px;
+			margin-left: 30px;
+		}
+`
+
+//Icons
+
+export const MailIcon = styled(MailOutlineOutlinedIcon)`
+	color: ${green};
+	margin-right: 10px;
+	`;
+
+export const NameIcon = styled(PersonOutlineIcon)`
+	color: ${green};
+	margin-right: 10px;
+	`;
+
+export const PassIcon = styled(LockOutlinedIcon)`
+	color: ${green};
+	margin-right: 10px;
+`;
+
+
+
+//Others
+export const AuthTitle = styled.h1`
+		font-size: ${fontH1};
+		color: ${green};
+		font-weight: 700;
+		text-align: center;
+`
+
+export const AuthLink = styled(Link)`
+	color: ${green};
+	text-decoration: none;
+	text-align: center;
+	margin: 20px auto;
+	font-size: ${fontP};
+	letter-spacing: 1px;
+`
+
+export const AuthBtn = styled.button`
+	padding: 10px;
+	border: 2px solid ${green};
+	border-radius: 15px;
+	color: ${green};
+	background: transparent;
+	font-size: ${fontH3};
+	text-align: center;
+`;
+
+export const AuthHelper = styled.p`
+	font-size: 12px;
+	margin-top: 20px;
+	color: white;
+	text-align: center;
+
+	> a {
+		text-decoration: none;
+		color: ${green};
+
+	}
+`

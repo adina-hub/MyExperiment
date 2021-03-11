a3836f55fa15f66f760f658030074062c600c70b
andreimediatack <andrei.manea@mediatack.de>
2021-03-11 14:46:09 +0200

    Added firebase signIn and signUp functionality

--- src/components/Pages/SignUp/SignUp.js
+++ src/components/Pages/SignUp/SignUp.js
@@ -18,75 +18,72 @@ import {
 	NameIcon,
 	PassIcon
 } from '../../../styles/auth';
+import { useAuth } from '../../../context/AuthContext';
 
 export default function SignUp() {
+	const { signUp } = useAuth();
 	return (
-		<>
-			<AuthContainer>
-				<AuthLogo src={logo} alt="" />
-				<AuthFormContainer dark="true" padding margin>
-					<AuthTitle>Register</AuthTitle>
-					<Formik
-						initialValues={{ email: '', name: '', password: '', password2: '' }}
-						onSubmit={async (values) => {
-							await new Promise((resolve) => setTimeout(resolve, 500));
-							alert(JSON.stringify(values, null, 2));
-						}}
-					>
-						<AuthForm>
-							<AuthLabel>Email</AuthLabel>
-							<AuthInputContainer>
-								<MailIcon />
-								<AuthInputField
-									name="email"
-									type="email"
-									placeholder="Type your email"
-									required
-								/>
-							</AuthInputContainer>
+		<AuthContainer>
+			<AuthLogo src={logo} alt="" />
+			<AuthFormContainer dark="true" padding margin>
+				<AuthTitle>Register</AuthTitle>
+				<Formik
+					initialValues={{ email: '', name: '', password: '', password2: '' }}
+					onSubmit={values => signUp(values.email, values.password, values.name)}
+				>
+					<AuthForm>
+						<AuthLabel>Email</AuthLabel>
+						<AuthInputContainer>
+							<MailIcon />
+							<AuthInputField
+								name="email"
+								type="email"
+								placeholder="Type your email"
+								required
+							/>
+						</AuthInputContainer>
 
-							<AuthLabel>Name</AuthLabel>
-							<AuthInputContainer>
-								<NameIcon />
-								<AuthInputField
-									name="name"
-									type="name"
-									placeholder="Type your name"
-									required
-								/>
-							</AuthInputContainer>
+						<AuthLabel>Name</AuthLabel>
+						<AuthInputContainer>
+							<NameIcon />
+							<AuthInputField
+								name="name"
+								type="name"
+								placeholder="Type your name"
+								required
+							/>
+						</AuthInputContainer>
 
-							<AuthLabel>Password</AuthLabel>
-							<AuthInputContainer>
-								<NameIcon />
-								<AuthInputField
-									name="password"
-									type="password"
-									placeholder="Type your password"
-									required
-								/>
-							</AuthInputContainer>
+						<AuthLabel>Password</AuthLabel>
+						<AuthInputContainer>
+							<NameIcon />
+							<AuthInputField
+								name="password"
+								type="password"
+								placeholder="Type your password"
+								required
+							/>
+						</AuthInputContainer>
 
-							<AuthLabel>Password</AuthLabel>
-							<AuthInputContainer>
-								<PassIcon />
-								<AuthInputField
-									name="password2"
-									type="password"
-									placeholder="Re-type your password"
-									required
-								/>
-							</AuthInputContainer>
+						<AuthLabel>Password</AuthLabel>
+						<AuthInputContainer>
+							<PassIcon />
+							<AuthInputField
+								name="password2"
+								type="password"
+								placeholder="Re-type your password"
+								required
+							/>
+						</AuthInputContainer>
 
-							<AuthLink>Forgot password?</AuthLink>
-							<AuthBtn type="submit">REGISTER</AuthBtn>
-						</AuthForm>
-					</Formik>
-					<AuthHelper>
-						Already have an account? <Link to="/signin">Sign In</Link>
-					</AuthHelper>
-				</AuthFormContainer>
-			</AuthContainer>
-		</>
+						<AuthLink>Forgot password?</AuthLink>
+						<AuthBtn type="submit">REGISTER</AuthBtn>
+					</AuthForm>
+				</Formik>
+				<AuthHelper>
+					Already have an account? <Link to="/signin">Sign In</Link>
+				</AuthHelper>
+			</AuthFormContainer>
+		</AuthContainer>
 	);
 }

8dc4c5bb921a5a54b76ebfa7e1204daa96581115
andreimediatack <andrei.manea@mediatack.de>
2021-03-11 14:45:17 +0200

    Added signIn, signUp, addEvent, addExperiment functionalities for firebase.

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,6 +1,6 @@
 import React, { useContext, useState, useEffect } from 'react';
-import { auth } from '../firebase';
-import firebase from 'firebase';
+import { useHistory } from 'react-router-dom';
+import { auth, db } from '../firebase';
 
 const AuthContext = React.createContext();
 
@@ -9,24 +9,42 @@ export function useAuth() {
 }
 
 export function AuthProvider({ children }) {
-	const [currentUser, setCurrentUser] = useState();
+	const [currentUser, setCurrentUser] = useState(null);
 	const [loading, setLoading] = useState(true);
-
-	function signup(email, password, name) {
+	const history = useHistory();
+	// GENERAL FUNCTIONALITIES
+	function signUp(email, password, name) {
 		return auth
 			.createUserWithEmailAndPassword(email, password)
-			.then(function (response) {
-				firebase.database().ref('User').push({
+			.then((userAuth) => {
+				db.collection('users').add({
 					email: email,
-					password: password,
-					id: auth.currentUser.uid,
-					fullname: name
-				});
-			});
+					uid: userAuth.user.uid,
+					name: name,
+					admin: false,
+					favourites: [],
+					events: []
+				}).then(() => {
+					history.push("/signin");
+				}).catch(err => alert(err))
+			}).catch(err => alert(err));
 	}
 
-	function login(email, password) {
-		return auth.signInWithEmailAndPassword(email, password);
+	function signIn(email, password) {
+		return auth.signInWithEmailAndPassword(email, password).then((userAuth) => {
+			db.collection('users').where("uid", "==", userAuth.user.uid).get()
+				.then((querySnapshot) => {
+					querySnapshot.forEach((doc) => {
+						let data = doc.data()
+						setCurrentUser({
+							email: data.email,
+							name: data.name,
+							uid: data.uid,
+							admin: data.admin,
+						})
+					})
+				})
+		});
 	}
 
 	function resetPassword(email) {
@@ -37,9 +55,37 @@ export function AuthProvider({ children }) {
 		return auth.signOut();
 	}
 
+	// ADMIN FUNCTIONALITIES
+
+	function addEvent(title, imgUrl, description, location, date, domains, places, steps) {
+		db.collection('events').add({
+			title: title,
+			imgUrl: imgUrl,
+			description: description,
+			location: location,
+			date: date,
+			domains: domains,
+			places: places,
+			steps: steps
+		}).then(() => console.log("ADDED EVENT!!!"));
+	}
+
+	function addExperiment(title, videoUrl, materials, domains, steps) {
+		db.collection('experiments').add({
+			title: title,
+			videoUrl: videoUrl,
+			materials: materials,
+			domains: domains,
+			steps: steps
+		}).then(() => console.log("ADDED EXPERIMENT!!!"));
+	}
+
+
 	useEffect(() => {
-		const unsubscribe = auth.onAuthStateChanged((user) => {
-			setCurrentUser(user);
+		const unsubscribe = auth.onAuthStateChanged((userAuth) => {
+			if (!userAuth) {
+				setCurrentUser(null);
+			}
 			setLoading(false);
 		});
 
@@ -48,10 +94,12 @@ export function AuthProvider({ children }) {
 
 	const value = {
 		currentUser,
-		signup,
-		login,
+		signUp,
+		signIn,
 		resetPassword,
-		logout
+		logout,
+		addEvent,
+		addExperiment
 	};
 
 	return (

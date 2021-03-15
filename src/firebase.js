4850e41ae758aee4770d53d4e1ec4a8b63802cdd
andreimediatack <andrei.manea@mediatack.de>
2021-03-15 13:56:04 +0200

    restructured firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,18 @@
-import firebase from 'firebase/app';
-import 'firebase/auth';
+import firebase from 'firebase';
 
-const app = firebase.initializeApp({
-	apiKey: 'AIzaSyBV-4r-_kAwm0rmkNCwH88f-Ape9u3vxwU',
-	authDomain: 'myexperiment-c6404.firebaseapp.com',
-	databaseURL: 'https://myexperiment-c6404-default-rtdb.firebaseio.com/',
-	projectId: 'myexperiment-c6404',
-	storageBucket: 'myexperiment-c6404.appspot.com',
-	messagingSenderId: '731232993558',
-	appId: '1:731232993558:web:7444b7b3378e39c596d037'
-});
+const firebaseConfig = {
+	apiKey: "AIzaSyBV-4r-_kAwm0rmkNCwH88f-Ape9u3vxwU",
+	authDomain: "myexperiment-c6404.firebaseapp.com",
+	databaseURL: "https://myexperiment-c6404-default-rtdb.firebaseio.com",
+	projectId: "myexperiment-c6404",
+	storageBucket: "myexperiment-c6404.appspot.com",
+	messagingSenderId: "731232993558",
+	appId: "1:731232993558:web:6dfcd1bd55e71d7096d037"
+};
 
-export const auth = app.auth();
 
-export default app;
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebaseApp.auth();
+
+export { db, auth };

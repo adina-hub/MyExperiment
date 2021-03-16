0e078158f50fc6e188a2e2f5920259dbeeacbf38
andreimediatack <andrei.manea@mediatack.de>
2021-03-16 16:27:35 +0200

    implemented GET from the database and filtering functionality

--- src/components/Pages/Experiments/Experiments.js
+++ src/components/Pages/Experiments/Experiments.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import {
 	PageContainer,
@@ -7,62 +7,64 @@ import {
 	PageHR,
 	PageSection,
 	PageSubtitle,
-	lightBg,
-	green
 } from '../../../styles/general';
 import Navbar from '../../Elements/Navbar/Navbar';
-import Experiment from './Experiment/Experiment';
 import Dropdown from '../../Elements/Dropdown/Dropdown';
-import { useParams } from 'react-router-dom';
+import { db } from '../../../firebase';
+import Experiment from '../../Elements/Experiment/Experiment';
 
 function Experiments() {
-	let { id } = useParams();
-	if (id) {
-		return <Experiment id={id} />;
-	} else {
-		return (
-			<PageContainer>
-				<Navbar></Navbar>
-				<PageSection fullHeight>
-					<PageTitle>Experiments</PageTitle>
-					<PageHR />
-					<PageDescription>Here you can find physics related experiments.</PageDescription>
-					<PageDescription>
-						From mechanics to optics and fluids, all in one place, each of them
-						having a video with explications from our teachers.
+	const [experiments, setExperiments] = useState([])
+	const [category, setCategory] = useState("All");
+	useEffect(() => {
+		db.collection("experiments").get().then(querySnapshot => {
+			let array = [];
+			querySnapshot.docs.map(doc => {
+				let id = doc.id;
+				let data = doc.data();
+				array.push({
+					id: id,
+					title: data.title,
+					steps: data.steps,
+					videoUrl: data.videoUrl,
+					domains: data.domains,
+					materials: data.materials
+				})
+			});
+			setExperiments(array);
+		});
+	}, []);
+
+	return (
+		<PageContainer>
+			<Navbar></Navbar>
+			<PageSection fullHeight>
+				<PageTitle>Experiments</PageTitle>
+				<PageHR />
+				<PageDescription>Here you can find physics related experiments.</PageDescription>
+				<PageDescription>
+					From mechanics to optics and fluids, all in one place, each of them
+					having a video with explications from our teachers.
 					</PageDescription>
-					<PageHR />
-					<Dropdown />
-				</PageSection>
-				<PageSection dark fullHeight>
-					<PageSubtitle>Category: All</PageSubtitle>
-					<ExperimentsList>
-						<ExperimentsItem>
-							<img
-								src="https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9vZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-								alt=""
-							/>
-							<p>How to make a sandwich</p>
-						</ExperimentsItem>
-						<ExperimentsItem>
-							<img
-								src="https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9vZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-								alt=""
-							/>
-							<p>How to make a sandwich</p>
-						</ExperimentsItem>
-						<ExperimentsItem>
-							<img
-								src="https://images.unsplash.com/photo-1482049016688-2d3e1b311543?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MXx8Zm9vZHxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
-								alt=""
-							/>
-							<p>How to make a sandwich</p>
-						</ExperimentsItem>
-					</ExperimentsList>
-				</PageSection>
-			</PageContainer>
-		);
-	}
+				<PageHR />
+				<Dropdown expCategories={experiments} expSetCategory={setCategory} />
+			</PageSection>
+			<PageSection dark fullHeight>
+				<PageSubtitle>Category: {category}</PageSubtitle>
+				<ExperimentsList>
+					{experiments.map(experiment => {
+						if (category === "All") {
+							return (<Experiment title={experiment.title} id={experiment.id} />)
+						} else if (experiment.domains[0] === category
+							|| experiment.domains[1] === category
+							|| experiment.domains[2] === category) {
+							return (<Experiment title={experiment.title} id={experiment.id} />)
+						}
+					})}
+				</ExperimentsList>
+			</PageSection>
+		</PageContainer>
+	);
 }
 
 export default Experiments;
@@ -74,22 +76,3 @@ const ExperimentsList = styled.div`
 	margin-top: 20px;
 `;
 
-const ExperimentsItem = styled.div`
-	display: flex;
-	background: ${lightBg};
-	color: ${green};
-	align-items: center;
-	border-radius: 5px;
-	margin-top: 20px;
-	> img {
-		flex: 0.3;
-		height: 75px;
-		filter: grayscale(100%);
-		object-fit: cover;
-	}
-
-	> p {
-		flex: 0.7;
-		text-align: center;
-	}
-`;

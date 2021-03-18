0f00b6f0d15feaa2e76c08e166e3940b9c443de9
andreimediatack <andrei.manea@mediatack.de>
2021-03-18 15:22:05 +0200

    Implemented GET Favorites

--- src/components/Pages/User/UserExperiments.js
+++ src/components/Pages/User/UserExperiments.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuth } from '../../../context/AuthContext';
+import { db } from '../../../firebase';
 import {
 	PageContainer,
 	PageSection,
@@ -12,16 +14,42 @@ import AdminNavbar from '../../Elements/AdminNavbar/AdminNavbar';
 import ListItem from '../../Elements/ListItem/ListItem';
 
 export default function UserExperiments() {
+
+	const [experiments, setExperiments] = useState([])
+	const { currentUser } = useAuth();
+
+
+
+	useEffect(() => {
+		const initialise = async () => {
+			await db.collection('users').where("uid", "==", currentUser.uid).get()
+				.then(querySnapshot => {
+					querySnapshot.forEach(async (doc) => {
+						let experimentsCopy = [];
+						for (const favorite of doc.data().favorites) {
+							const result = await db.collection("experiments").doc(favorite).get().then(doc => ({
+								id: doc.id,
+								name: doc.data().title
+							}));
+							experimentsCopy.push(result);
+						}
+						return setExperiments(experimentsCopy);
+					});
+				});
+		}
+		initialise();
+	}, [])
+
 	return (
 		<PageContainer>
 			<AdminNavbar />
 			<PageSection fullHeight>
-				<PageTitle>Experiments</PageTitle>
+				<PageTitle>Favorites</PageTitle>
 				<PageHR />
 				<ExperimentsList>
-					<ListItem name="Experiment One" url="/experiments/dadasd" />
-					<ListItem name="Experiment Two" url="/experiments/dadasd" />
-					<ListItem name="Experiment Three" url="/experiments/dadasd" />
+					{experiments.map((experiment, i) => (
+						<ListItem key={i} name={experiment.name} url={`/experiments/${experiment.id}`} />
+					))}
 				</ExperimentsList>
 				<PageBtnContainer>
 					<PageLinkBtn to="/user">Go back</PageLinkBtn>

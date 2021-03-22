cdeb3b7c9c5a20c2a5b25ac22b2c352266794997
andreimediatack <andrei.manea@mediatack.de>
2021-03-22 13:12:03 +0200

    Added EDIT route and renamed routes

--- src/App.js
+++ src/App.js
@@ -14,30 +14,32 @@ import User from './components/Pages/User/User';
 import Event from './components/Pages/Events/Event/Event';
 import UserExperiments from './components/Pages/User/UserExperiments';
 import UserEvents from './components/Pages/User/UserEvents';
-import Test from './components/Pages/Test/Test';
 import { AuthProvider } from './context/AuthContext';
 import Experiment from './components/Pages/Experiments/Experiment/Experiment';
+import AdminRoute from './routes/AdminRoute';
+import UserRoute from './routes/UserRoute';
 function App() {
 	return (
 		<Router>
 			<AuthProvider>
 				<Switch>
-					<Route path="/test" component={Test} exact />
-					<Route path="/signin" component={SignIn} />
-					<Route path="/signup" component={SignUp} />
+					<Route path="/signIn" component={SignIn} />
+					<Route path="/signUp" component={SignUp} />
 					<Route path="/" component={Home} exact />
 					<Route path="/experiments" component={Experiments} exact />
 					<Route path="/experiments/:id" component={Experiment} />
 					<Route path="/events" component={Events} exact />
 					<Route path="/events/:id" component={Event} />
-					<Route path="/admin" component={AdminHome} />
-					<Route path="/experimentsList" component={AdminExperiments} />
-					<Route path="/eventsList" component={AdminEvents} />
-					<Route path="/newExperiment" component={NewExperiment} />
-					<Route path="/newEvent" component={NewEvent} />
-					<Route path="/user" component={User} />
-					<Route path="/userExpList" component={UserExperiments} />
-					<Route path="/userEvtList" component={UserEvents} />
+					<AdminRoute path="/admin" exact component={AdminHome} />
+					<AdminRoute path="/admin/experiments" exact component={AdminExperiments} />
+					<AdminRoute path="/admin/experiments/:id" component={NewExperiment} />
+					<AdminRoute path="/admin/events" exact component={AdminEvents} />
+					<AdminRoute path="/admin/events/:id" component={NewEvent} />
+					<AdminRoute path="/admin/experiments/new" component={NewExperiment} />
+					<AdminRoute path="/admin/events/new" component={NewEvent} />
+					<UserRoute path="/user" exact component={User} />
+					<UserRoute path="/user/experiments" component={UserExperiments} />
+					<UserRoute path="/user/events" component={UserEvents} />
 				</Switch>
 			</AuthProvider>
 		</Router>

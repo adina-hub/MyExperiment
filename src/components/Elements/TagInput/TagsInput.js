85e47ee83300fac33ead8a7b470508d3eae5aafc
andreimediatack <andrei.manea@mediatack.de>
2021-03-22 13:16:28 +0200

    Modified to take values from Formik initialValues

--- src/components/Elements/TagInput/TagsInput.js
+++ src/components/Elements/TagInput/TagsInput.js
@@ -2,15 +2,17 @@ import { Field } from 'formik'
 import React from 'react'
 import Tags from './Tags/Tags'
 
-function TagsInput({ name, id }) {
-
+function TagsInput({ name }) {
+    const fieldName = name;
     return (
-        <Field name={name} id={id} type="text">
-            {({ field: { name }, form: { setFieldValue } }) => (
-                <Tags
+        <Field name={fieldName} id={fieldName} type="text">
+            {({ field: { value }, form: { setFieldValue } }) => {
+                console.log(value);
+                return (<Tags
+                    tagsValue={value}
                     tagsCount={tag => setFieldValue(name, tag)}
-                />
-            )}
+                />)
+            }}
         </Field>
     )
 }

90cf2f6648b2721a364c7903940c1b40199751a9
andreimediatack <andrei.manea@mediatack.de>
2021-03-22 13:11:35 +0200

    Implemented Admin and User routes

---
+++ src/routes/AdminRoute.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import { useAuth } from '../context/AuthContext'
+
+export default function AdminRoute({ component: Component, ...rest }) {
+    const { currentUser } = useAuth()
+    console.log(currentUser);
+    return (
+        <Route {...rest} render={props => {
+            return currentUser && currentUser.admin ? <Component {...props} /> : <Redirect to="/signin" />
+        }}>
+
+        </Route>
+    )
+}

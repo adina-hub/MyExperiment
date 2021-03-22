673cb28a7aff928cbf931353e55002e52dc9e883
andreimediatack <andrei.manea@mediatack.de>
2021-03-22 13:19:52 +0200

    Changed button names and displayed user info

--- src/components/Pages/User/User.js
+++ src/components/Pages/User/User.js
@@ -9,18 +9,22 @@ import {
 	UserBtnContainer,
 	UserField,
 	UserFieldName,
-	UserFieldValue
+	UserFieldValue,
+	green
 } from '../../../styles/general.js';
-import AdminNavbar from '../../Elements/AdminNavbar/AdminNavbar';
+import Navbar from '../../Elements/Navbar/Navbar.js';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import EventNoteIcon from '@material-ui/icons/EventNote';
+import styled from 'styled-components';
 
 export default function User() {
 	const { currentUser } = useAuth();
 
 	return (
 		<PageContainer>
-			<AdminNavbar />
+			<Navbar />
 			<PageSection fullHeight>
-				<PageTitle>Welcome, {currentUser.admin ? "admin" : currentUser.name}</PageTitle>
+				<PageTitle>Welcome, {currentUser.name}</PageTitle>
 				<PageHR />
 				<UserField>
 					<UserFieldName>Name:</UserFieldName>
@@ -31,14 +35,29 @@ export default function User() {
 					<UserFieldValue>{currentUser.email}</UserFieldValue>
 				</UserField>
 				<UserBtnContainer>
-					<PageLinkBtn large="true" to="/userExpList">
-						Favorites
-					</PageLinkBtn>
-					<PageLinkBtn large="true" to="/userEvtList">
-						Booked events
-					</PageLinkBtn>
+					<UserBtn large="true" to="/user/experiments">
+						<FavoriteIcon />
+						Experiments
+					</UserBtn>
+					<UserBtn large="true" to="/user/events">
+						<EventNoteIcon />
+						Events
+					</UserBtn>
 				</UserBtnContainer>
 			</PageSection>
 		</PageContainer>
 	);
 }
+
+const UserBtn = styled(PageLinkBtn)`
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	margin-bottom: 20px;
+	> .MuiSvgIcon-root{
+		fill: ${green} !important;
+		margin-right: 5px;
+	}
+	
+`
+
